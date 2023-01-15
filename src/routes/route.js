e3a4d14f71a86c3a8c7f871892f589e62d1075f7
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-01-15 19:09:48 +0530

    Done

---
+++ src/routes/route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/getdata");
+const data = require("../middleware/middleware");
+router.get("/assets", data.myFunction, controller.myapi);
+module.exports = router;
